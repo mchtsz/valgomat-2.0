cb9dca82f00815b98c0f46ebbac63d38e2509557
Mchtsz <mhatland206@gmail.com>
2023-10-06 11:31:36 +0200

    styled, copied code to håvard, not finished

--- valgomat.js
+++ valgomat.js
@@ -3,114 +3,114 @@ const questions = [
   {
     question: "Bør vi ha lekser på skolen?",
     enig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
+      FRP: 2,
+      H: 3,
+      V: 1,
+      KRF: 1,
+      SP: 2,
+      MDG: 5,
+      AP: 4,
     },
     uenig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
+      FRP: 1,
+      H: 2,
+      V: 3,
+      KRF: 3,
+      SP: 4,
+      MDG: 5,
+      AP: 2,
     },
   },
   {
     question: "Bør vi gi pengestøtte til bønder?",
     enig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
+      FRP: 4,
+      H: 3,
+      V: 2,
+      KRF: 5,
+      SP: 5,
+      MDG: 1,
+      AP: 2,
     },
     uenig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
+      FRP: 1,
+      H: 2,
+      V: 3,
+      KRF: 1,
+      SP: 2,
+      MDG: 3,
+      AP: 4,
     },
   },
   {
     question: "Bør vi gi pengestøtte til kunstnere?",
     enig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
+      FRP: 3,
+      H: 4,
+      V: 2,
+      KRF: 1,
+      SP: 2,
+      MDG: 5,
+      AP: 5,
     },
     uenig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
+      FRP: 2,
+      H: 1,
+      V: 3,
+      KRF: 4,
+      SP: 4,
+      MDG: 1,
+      AP: 1,
     },
   },
   {
     question: "Bør vi senke skattene?",
     enig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
+      FRP: 5,
+      H: 4,
+      V: 3,
+      KRF: 2,
+      SP: 2,
+      MDG: 1,
+      AP: 1,
     },
     uenig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
+      FRP: 1,
+      H: 2,
+      V: 3,
+      KRF: 4,
+      SP: 4,
+      MDG: 5,
+      AP: 5,
     },
   },
   {
     question: "Bør vi ha strengere straffer?",
     enig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
-      SP: 0,
+      FRP: 4,
+      H: 3,
+      V: 2,
+      KRF: 2,
+      SP: 1,
       MDG: 0,
-      AP: 0,
+      AP: 2,
     },
     uenig: {
       FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
+      H: 1,
+      V: 2,
+      KRF: 3,
+      SP: 4,
+      MDG: 5,
+      AP: 3,
     },
   },
   {
     question: "Bør vi finne nye oljefelt?",
     enig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
+      FRP: 5,
+      H: 4,
+      V: 3,
       KRF: 0,
       SP: 0,
       MDG: 0,
@@ -122,105 +122,84 @@ const questions = [
       V: 0,
       KRF: 0,
       SP: 0,
-      MDG: 0,
-      AP: 0,
+      MDG: 5,
+      AP: 4,
     },
   },
   {
     question: "Bør vi få flere private sykehus?",
     enig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
+      FRP: 4,
+      H: 3,
+      V: 5,
+      KRF: 2,
       SP: 0,
       MDG: 0,
-      AP: 0,
+      AP: 1,
     },
     uenig: {
       FRP: 0,
       H: 0,
       V: 0,
       KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
+      SP: 5,
+      MDG: 5,
+      AP: 5,
     },
   },
   {
     question: "Bør vi beholde kontantstøtten?",
     enig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
+      FRP: 4,
+      H: 3,
+      V: 2,
+      KRF: 2,
       SP: 0,
       MDG: 0,
-      AP: 0,
+      AP: 1,
     },
     uenig: {
       FRP: 0,
       H: 0,
       V: 0,
       KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
-    },
-  },
-  {
-    question: "Bør vi beholde kontantstøtten?",
-    enig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
-    },
-    uenig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
+      SP: 5,
+      MDG: 5,
+      AP: 5,
     },
   },
   {
     question:
       "Bør leger ha muligheten til å reservere seg mot å henvise til abort?",
     enig: {
-      FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
+      FRP: 4,
+      H: 4,
+      V: 2,
+      KRF: 4,
       SP: 0,
       MDG: 0,
-      AP: 0,
+      AP: 2,
     },
     uenig: {
       FRP: 0,
       H: 0,
       V: 0,
       KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
+      SP: 5,
+      MDG: 5,
+      AP: 3,
     },
   },
   {
     question: "Bør vi ta imot flere flyktninger?",
     enig: {
       FRP: 0,
-      H: 0,
-      V: 0,
-      KRF: 0,
-      SP: 0,
-      MDG: 0,
-      AP: 0,
+      H: 4,
+      V: 3,
+      KRF: 2,
+      SP: 1,
+      MDG: 5,
+      AP: 5,
     },
     uenig: {
       FRP: 7,
@@ -318,4 +297,50 @@ function showResult() {
   for (let party in sortedPartyScores) {
     resultBox.innerHTML += party + ": " + sortedPartyScores[party] + "<br>";
   }
+}
+
+function createProgressText(container, parti) {
+  const progressTekst = document.createElement("div");
+  progressTekst.setAttribute("class", "partiTekst");
+  progressTekst.style.fontWeight = "bold";
+  progressTekst.style.fontSize = "22px";
+  progressTekst.style.marginBottom = "3%";
+  progressTekst.style.marginTop = "3%";
+  progressTekst.style.color = parti.partiFarge;
+  progressTekst.innerText = parti.navn;
+  container.appendChild(progressTekst);
+}
+
+function createProgressDivBar(container, parti) {
+  const progressDiv = document.createElement("div");
+  progressDiv.setAttribute("class", "progress");
+  progressDiv.style.width = "90%";
+  progressDiv.style.margin = "auto";
+  container.append(progressDiv);
+
+  const progressBar = document.createElement("div");
+  progressBar.setAttribute(
+    "class",
+    "progress-bar",
+    "role",
+    "progressbar",
+    "aria-valuenow",
+    (parti.poeng / Object.keys(spørsmål).length) * 100,
+    "aria-valuemin",
+    "0",
+    "aria-valuemax",
+    "100"
+  );
+  progressBar.style.width = "0%";
+  progressBar.innerText = "0%";
+  progressBar.style.backgroundColor = parti.partiFarge;
+  progressDiv.appendChild(progressBar);
+  setTimeout(() => {
+    progressBar.style.transition = "width 1s ease-in-out";
+    progressBar.style.width =
+      (parti.poeng / Object.keys(spørsmål).length) * 100 + "%";
+    progressBar.innerText =
+      Number(Math.round((parti.poeng / Object.keys(spørsmål).length) * 100)) +
+      "%";
+  }, 250);
 }
